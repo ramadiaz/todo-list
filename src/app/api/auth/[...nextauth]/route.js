f60642f000251edcd3d7e30063ca8fdc89a0442c
Rama Diaz <ramadiaz221@gmail.com>
2024-03-10 20:14:42 +0700

    Prisma setup

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/app/libs/prisma";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        password: { label: "Password", type: "password" },
+        email: { label: "Email", type: "email" },
+      },
+      async authorize(credentials) {
+        if (!credentials.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        if(!user) {
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
+
+        if(!passwordsMatch) {
+          throw new Error("Incorrect email or password")
+        }
+        
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV !== "development",
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
