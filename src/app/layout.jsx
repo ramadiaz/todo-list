d9835486ae6b24bee09f40a8ad944b0cab248921
Rama Diaz <ramadiaz221@gmail.com>
2024-03-10 20:39:08 +0700

    Next UI Initialization

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "../../context/Provider";
+import NavbarComponent from "@/components/NavbarComponent";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Provider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Providers>
+            <NavbarComponent />
+            {children}
+          </Providers>
+        </body>
       </Provider>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
+
+'use client'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>
+     <div className="h-screen">
       hahahahaha
      </div>
     </main>
